d1d9c8cde1913000c47cb51d544f8f39d436e285
Siddhant <siddhantp457@gmail.com>
2022-08-14 00:46:20 +0530

    Added: FAQ Section

--- js/index.js
+++ js/index.js
@@ -7,3 +7,22 @@ const navbarItems = document.querySelector(".navbar_items");
 toggleBtn.addEventListener("click", () => {
     navbarItems.classList.toggle("active");
 });
+
+// Accordion code
+
+document.querySelectorAll(".accordion__heading").forEach(elmt => {
+  elmt.addEventListener("click", () => {
+    const accordionContent = elmt.nextElementSibling;
+    elmt.classList.toggle(".accordion__heading--active");
+    elmt.querySelector("ion-icon").style.transform = "rotate(180deg)";
+
+    if (elmt.classList.contains(".accordion__heading--active")) {
+      accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
+      // elmt.querySelector("ion-icon").style.transform = "rotate(180deg)";
+    } else {
+      accordionContent.style.maxHeight = 0;
+      elmt.querySelector("ion-icon").style.transform = "rotate(0deg)";
+    }
+
+  })
+})
